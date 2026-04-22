d4e63f308e6ac799d9f7f8194ee8dd3bcb3e17bf
WizardWhu <sam.sundue@gmail.com>
2026-04-22 12:36:09 -0400

    Fixed lighting and added interactable script

--- Abandoned Server Game Design MDA/Assets/Scripts/ButtonInteractable.cs
+++ Abandoned Server Game Design MDA/Assets/Scripts/ButtonInteractable.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ButtonInteracable : Interactable
 {
+    public UnityEvent OnPressed;
     public override void Interact(CCPlayer ccPlayer)
     {
-
+        OnPressed?.Invoke();
     }
 }
